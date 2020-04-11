5424c73ea49e809ee88f05c85f0835f39516c5af
ljdatasci <ljdatasci@gmail.com>
2020-04-11 14:12:47 -0400

    added createDataset

--- index.js
+++ index.js
@@ -1,35 +1,85 @@
-const {Storage} = require('@google-cloud/storage');
+//const {Storage} = require('@google-cloud/storage');
 const {BigQuery} = require('@google-cloud/bigquery');
 
-const storage = new Storage({});
-const bigquery = new BigQuery();
+const options = {
+    keyFilename: './contact-form-253313-2ecf6555cb3d.json',
+    projectId: 'contact-form-253313',
+  };
+
+//const storage = new Storage();
+const bigquery = new BigQuery(options);
 
 const bucketName = 'big-query-upload';
 const fileName = 'Maryland_Operating_Budget.csv';
-// async function createBucket(){
-//   await storage.createBucket(bucketName);
-//   console.log(`Bucket ${bucketName} created.`);
-// }
+const datasetID = 'MD_Operating_Budget';
+const tableID = 'MD_Operating_Budget';
 
-// createBucket().catch(console.error);
+schema = [
+  {name: 'Organization_Code', type: 'STRING'},
+  {name: 'Organization_Sub_Code', type: 'STRING'},
+  {name: 'Type', type: 'STRING'},
+  {name: 'Fiscal_Year', type: 'STRING'},
+  {name: 'Agency_Code', type: 'STRING'},
+  {name: 'Unit_Code', type: 'STRING'},
+  {name: 'Unit_Name', type: 'STRING'},
+  {name: 'Program_Code', type: 'STRING'},
+  {name: 'Program_Name', type: 'STRING'},
+  {name: 'Subprogram_Code', type: 'STRING'},
+  {name: 'Subprogram_Name', type: 'STRING'},
+  {name: 'Object_Code', type: 'STRING'},
+  {name: 'Object_Name', type: 'STRING'},
+  {name: 'Comptroller_Subobject_Code', type: 'STRING'},
+  {name: 'Agency_Subobject_Code', type: 'STRING'},
+  {name: 'Agency_Subobject_Name', type: 'STRING'},
+  {name: 'Fund_Type_Name', type: 'STRING'},
+  {name: 'Budget', type: 'INTEGER'},
+  {name: 'Descripting', type: 'STRING'},
+  {name: 'Category', type: 'INTEGER'},
+  {name: 'Category_Title', type: 'STRING'}
+]
 
-// async function uploadFile(){
-//   await storage.bucket(bucketName).upload(fileName, {
-//     gzip: true,
-//     metadata: {
-//       cacheControl: 'public, max-age=31536000',
-//     },
-//   });
 
-//   console.log(`${fileName} uploaded to ${bucketName}.`);
-// }
+async function createBucket(){
+  await storage.createBucket(bucketName);
+  console.log(`Bucket ${bucketName} created.`);
+}
 
-// uploadFile().catch(console.error);
+async function uploadFile(){
+  await storage.bucket(bucketName).upload(fileName, {
+    gzip: true,
+    metadata: {
+      cacheControl: 'public, max-age=31536000',
+    },
+  });
+
+  console.log(`${fileName} uploaded to ${bucketName}.`);
+}
+
+async function createDataset(){
+ const dsOptions ={
+     location: 'US',
+ };
+
+ const [dataset] = await bigquery.createDataset(datasetID, dsOptions);
+ console.log(`Dataset ${dataset.id} created.`); 
+}
+
+async function createTable(){
+  
+ const options = {
+   schema: schema,
+   location: 'US',
+ };
+
+ const [table] = await bigquery
+  .dataset(datasetID)
+  .createTable(tableID, options);
+
+  console.log(`Table ${table.id} created.`);
+
+}
 
 async function loadCSVFromGCS(){
-  const datasetID = 'MD_Operating_Budget';
-  const tableID = 'MD_Operating_Budget';
-
   const metadata = {
     sourceFormat: 'CSV',
     skipLeadingRows: 1,
@@ -73,4 +123,5 @@ async function loadCSVFromGCS(){
   }
 }
 
-loadCSVFromGCS().catch(console.error);
+//createDataset();
+//createTable();
